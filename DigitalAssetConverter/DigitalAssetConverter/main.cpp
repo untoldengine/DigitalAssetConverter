82307f655779ffba3ec3a889da32fee4e58dd14a
Harold Serrano <harold.serrano@untoldengine.com>
2020-02-29 18:23:37 -0700

    Added initial file

---
+++ DigitalAssetConverter/DigitalAssetConverter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DigitalAssetConverter
+//
+//  Created by Harold Serrano on 2/29/20.
+//  Copyright © 2020 Untold Engine Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
